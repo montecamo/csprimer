397e701c350d593cbdc2214ab8dd686257d1b1b6
montecamo <merrychristek@gmail.com>
2023-07-02 14:30:01 +0300

    sum-to-n optimized

--- computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
+++ computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
@@ -2,12 +2,9 @@ section .text
 global sum_to_n
 
 sum_to_n:
-  xor eax, eax
-  jmp sum
-
-sum:
-  add eax, edi
-  sub edi, 1
-  cmp edi, 0
-  jg sum 
-	ret
+  mov eax, edi
+  inc eax
+  mul edi
+  mov ecx, 2
+  div ecx
+  ret
